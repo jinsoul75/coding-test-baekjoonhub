c6d184c9334581ab3ac7e5f9237602120463e8a4
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-04-22 08:56:33 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -5,32 +5,32 @@ function solution(array, commands) {
   // array.slice(commands[2][0]-1,commands[2][1])
 
   //자르기
-  // for(let i=0; i<commands.length; i++){
-  //   answer.push(array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=>a-b)[commands[i][2]-1])
-  // }
   for(let i=0; i<commands.length; i++){
-    answer.push(quick(array.slice(commands[i][0]-1,commands[i][1]))[commands[i][2]-1])
+    answer.push(array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=>a-b)[commands[i][2]-1])
   }
+  // for(let i=0; i<commands.length; i++){
+  //   answer.push(quick(array.slice(commands[i][0]-1,commands[i][1]))[commands[i][2]-1])
+  // }
   return answer;
 }
 
-function quick(arr){
-  if(arr.length<=1) return arr;
+// function quick(arr){
+//   if(arr.length<=1) return arr;
 
-  const pivot = arr[0]; 
-  const left = [];
-  const right = [];
+//   const pivot = arr[0]; 
+//   const left = [];
+//   const right = [];
 
-  for(let i=1; i<arr.length; i++){ 
-    if(arr[i]<=pivot){
-      left.push(arr[i])
-    }else {
-      right.push(arr[i])
-    }
-  }
+//   for(let i=1; i<arr.length; i++){ 
+//     if(arr[i]<=pivot){
+//       left.push(arr[i])
+//     }else {
+//       right.push(arr[i])
+//     }
+//   }
 
-  const lSorted = quick(left);
-  const rSorted = quick(right);
+//   const lSorted = quick(left);
+//   const rSorted = quick(right);
   
-  return [...lSorted,pivot,...rSorted]
-}
+//   return [...lSorted,pivot,...rSorted]
+// }
