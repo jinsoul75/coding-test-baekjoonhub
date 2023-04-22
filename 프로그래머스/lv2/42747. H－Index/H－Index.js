22e1e5c0546d9674f1577b2f05f5d858e46e5497
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-04-22 23:44:46 +0900

    [level 2] Title: H-Index, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -0,0 +1,12 @@
+function solution(citations){
+  const arr = citations.sort((a,b)=>b-a)
+
+
+  let b = 1;
+
+  while(arr.filter((el)=>el>=b).length>=b){ //n번이상 인용된 논문의 갯수 >= n편이상일때까지
+    arr.filter((el)=>el>=b).length //인용 n편 이상인 논문의 갯수
+    b++
+  }
+  return b-1
+}
