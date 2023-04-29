b581dc04e3adfdf9877eb32baacc1c432178799b
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-04-29 20:19:59 +0900

    [level 2] Title: 올바른 괄호, Time: 6.13 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -3,11 +3,12 @@ function solution(s){
     let result = [];
     // 문자열을 돌면서 왼쪽이면 넣어뒀다가 오른쪽 만나면 왼쪽을 뺀다
     if(s[0]===")") return false
+    
     for(i=0; i<=s.length; i++){
         if(s[i]==="("){
             result.push(s[i])
-        } else if(s[i]===")"){
-            result.pop()
+        } else if(s[i]===")"&&result.pop() !== "("){
+            return false;
         }
     }
 
