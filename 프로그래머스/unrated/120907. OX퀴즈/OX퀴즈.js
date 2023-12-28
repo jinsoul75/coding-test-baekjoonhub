c2e9385f4120d55f06a614d886cfa936fb476dd0
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2023-12-28 14:28:43 +0900

    [level unrated] Title: OX퀴즈, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.js
@@ -1,15 +1,6 @@
 function solution(quiz) {
     var answer = [];
-    quiz.forEach(el => {
-        const split = el.split(" ")
-        if(split[1] === "+"){
-           if(Number(split[0])+Number(split[2]) === Number(split[4])) answer.push("O")
-           else answer.push("X")
-        }else {
-           if(Number(split[0])-Number(split[2]) === Number(split[4])) answer.push("O")
-           else answer.push("X")
-        }
-
-    })
-    return answer;
+    const pro = quiz.map(el => el.split(" "))
+    
+    return pro;
 }
--- 프로그래머스/unrated/120907. OX퀴즈/README.md
+++ 프로그래머스/unrated/120907. OX퀴즈/README.md
@@ -1,6 +1,6 @@
 # [level unrated] OX퀴즈 - 120907 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120907) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 4일 14:28:26
+2023년 12월 4일 14:28:42
 
 ### 문제 설명
 
