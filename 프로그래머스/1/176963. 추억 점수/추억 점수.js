d28135339b31b937a32ca0e2a952fe5cfefbcbfa
JINSOUL KIM <80370226+jinsoul75@users.noreply.github.com>
2024-02-09 01:47:22 +0900

    [level 1] Title: 추억 점수, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,18 @@
+function solution(name, yearning, photo) {
+    var answer = [];
+    const map = new Map()
+    
+    for(let i=0; i<name.length; i++){
+        map.set(name[i],yearning[i])
+    }
+    
+    for(let i=0; i<photo.length; i++){
+        let sum = 0
+        for(let j=0; j<photo[i].length; j++){
+            if(map.has(photo[i][j])) sum += map.get(photo[i][j])
+        }
+        answer.push(sum)
+    }
+    
+    return answer;
+}
